f86f6c3170f4b7a09ac0ab2f92ff736fa2aca2af
mamekhadi <109413660+mamekhadi@users.noreply.github.com>
2022-08-03 18:20:25 +0200

    initial import

---
+++ frontend/README.md
@@ -0,0 +1,29 @@
+## Configuration du projet
+```
+npm install
+```
+
+### Compile et recharge à chaud pour le développement
+```
+npm run serve
+```
+
+### Compile et minifies pour la production
+```
+npm run build
+```
+
+### Peluche et corrige les fichiers
+```
+npm run lint
+```
+
+### Personnaliser la configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Nous avons eu à creer trois utilisateurs
+### L'un avec comme username: sall et comme password: sall
+### Le deuxieme avec comme username: diaw et comme password: diaw
+### Le troisieme avec comme username: dieng et comme password: dieng123
+
+
---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <h1 class="card__title">Vente</h1>
+    <p class="card__subtitle">Bienvenue dans notre espace de vente d'articles</p>
+    <p class="card__site">Pour plus d'information sur le site veuillez </p>
+    <p class="card__lien" ><a  href="https://lamis-boutique.fr/fr/nouveaux-produits">Consulter le site</a></p>
+    <p>{{user.prenom}} {{user.nom}} {{user.email}}</p>
+    <p class="card__photo">
+      <img  :src="require('@/assets/logo2.jpg')"/>
+   </p>
+    <div class="form-row">
+      <button @click="logout()" class="button">
+        Déconnexion
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'Profile',
+  mounted: function () {
+    console.log(this.$store.state.user);
+    if (this.$store.state.user.userId == -1) {
+      this.$router.push('/');
+      return ;
+    }
+    this.$store.dispatch('getUserInfos');
+  },
+  computed: {
+    ...mapState({
+      user: 'userInfos',
+    })
+  },
+  methods: {
+    logout: function () {
+      this.$store.commit('logout');
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>>
