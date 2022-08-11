d477d01271eb3278c89f89d6e3647489623f6520
EspoirNorbert <espoirnorbert@gmail.com>
2022-08-11 02:10:17 +0100

    Ajout du dossier test

---
+++ test/src/router/index.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Login from "@/views/Login.vue";
+import Profile from "@/views/Profile.vue";
+
+
+
+const routes = [
+  { 
+    name: 'login',
+    path: '/', 
+    component: Login,
+  },
+  { 
+    name: 'profile',
+    path: '/profile', 
+    component: Profile, 
+    props:true 
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router;
